e430433071df5a7fab942225806d9a7db243120b
Donovan So <donovanso@yahoo.com.hk>
2023-01-26 17:44:21 -0500

    add code for handling uri

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,29 @@
-import * as vscode from 'vscode';
-import { FileExplorer } from './fileExplorer';
+import * as vscode from "vscode";
+import { UriHandler } from "./UriHandler";
+import { FileExplorer } from "./fileExplorer";
 
-export function activate(context: vscode.ExtensionContext) {
-	new FileExplorer(context);
+export async function activate(context: vscode.ExtensionContext) {
+  console.log("Extension started");
+
+  new FileExplorer(context);
+
+  // Uri handling
+  let disposable = vscode.commands.registerCommand(
+    "bricksDesignToCode.handleUri",
+    async () => {
+      const uriHandler = new UriHandler();
+
+      context.subscriptions.push(vscode.window.registerUriHandler(uriHandler));
+
+      const uri = await vscode.env.asExternalUri(
+        vscode.Uri.parse(`${vscode.env.uriScheme}://bricks.d2c-vscode`)
+      );
+
+      console.log(
+        `Starting to handle Uris. Open ${uri} in your browser to test.`
+      );
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
